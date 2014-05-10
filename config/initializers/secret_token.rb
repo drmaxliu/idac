01583faaca1e6531c33044f45c1e00578796f33c
Max Liu <dr.max.liu@gmail.com>
2014-05-10 01:34:41 -0700

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idac::Application.config.secret_token = 'bf466c271c7bac0e8af2be5605b318e555472a4f5d5d531067f2cc891f5d0aa740125ea75844fe7c3e9ca89ab0508275b71982cff58058649778f5e41303e0ec'
