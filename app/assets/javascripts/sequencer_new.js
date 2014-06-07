9c435db9d1b3d1c10e90459a56c52ccff9d449e7
Max Liu <dr.max.liu@gmail.com>
2014-06-07 08:50:47 -0700

    First Trial 12

--- app/assets/javascripts/sequencer_new.js
+++ app/assets/javascripts/sequencer_new.js
@@ -313,6 +313,7 @@ $(document).ready(function() {
       status = '0';
     } else {
       user_answer = $('#user_answer').html().split('||');
+      alert($('#user_answer').html());
       if(user_answer.length>1) {
         status = user_answer.shift();
         status.replace(/\r/g, '');
